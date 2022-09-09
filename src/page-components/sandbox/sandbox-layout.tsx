5d4441d17edce45bc7c176f444829ce15c144b15
Clement Murphy <clemmurphy@gmail.com>
2022-09-09 16:56:03 +0100

    add some sandbox code

--- src/page-components/sandbox/sandbox-layout.tsx
+++ src/page-components/sandbox/sandbox-layout.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
 import { useDrag, useDrop } from 'react-dnd'
 import { DraggableItemTypes } from 'constants/constants'
+import { useState } from 'react'
 
 const CenterEverythingDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 18px;
   height: 100vh;
   width: 100vw;
 `
@@ -15,6 +17,7 @@ export function SandboxLayout() {
     <CenterEverythingDiv>
       <DraggableComponent />
       <DroppableComponent />
+      <DraggableComponent2 />
     </CenterEverythingDiv>
   )
 }
@@ -22,9 +25,16 @@ export function SandboxLayout() {
 function DraggableComponent() {
   const [{ isDragging }, drag] = useDrag(() => ({
     type: DraggableItemTypes.CARD,
+    item: { value: 2 },
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging(),
     }),
+    options: {
+      dropEffect: 'copy',
+    },
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult()
+    },
   }))
 
   return (
@@ -35,24 +45,78 @@ function DraggableComponent() {
         fontSize: 25,
         fontWeight: 'bold',
         cursor: 'move',
+        background: 'lightblue',
+        padding: 20,
       }}
     >
-      <p>DRAG THIS</p>
+      <p>+ 2</p>
     </div>
   )
 }
 
-function DroppableComponent() {
-  const [collectedProps, drop] = useDrop(() => ({
-    accept: DraggableItemTypes.CARD,
+function DraggableComponent2() {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: DraggableItemTypes.CARD,
+    item: { value: -1 },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+    options: {
+      dropEffect: 'copy',
+    },
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult()
+    },
   }))
 
   return (
     <div
-      style={{ height: 100, width: 100, backgroundColor: 'rebeccapurple' }}
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+        fontWeight: 'bold',
+        cursor: 'move',
+        background: 'lightblue',
+        padding: 20,
+      }}
+    >
+      <p>- 1</p>
+    </div>
+  )
+}
+
+let dropCount = 0
+
+function DroppableComponent() {
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: DraggableItemTypes.CARD,
+    drop: (props: any, monitor: any) => {
+      const item = monitor.getItem()
+      dropCount = dropCount + item.value
+    },
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+      canDrop: !!monitor.canDrop(),
+    }),
+  }))
+
+  const color = isOver ? 'lightgreen' : 'lightpink'
+
+  return (
+    <div
+      style={{
+        height: 200,
+        width: 200,
+        backgroundColor: color,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 32,
+      }}
       ref={drop}
     >
-      Drop Target
+      <p>{dropCount}</p>
     </div>
   )
 }
